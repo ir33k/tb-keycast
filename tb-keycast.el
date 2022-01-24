5b8adc09cdeb1e58e9fd54a91fb6e3873cc65861
yantar92 <yantar92@gmail.com>
2022-01-24 16:36:47 +0800

    tb-keycast--update: Use `format' to print `this-command' symbol
    Co-authored-by: ir33k <49790836+ir33k@users.noreply.github.com>

--- tb-keycast.el
+++ tb-keycast.el
@@ -102,9 +102,7 @@ Force update of mode-line and by that udate tab-bar line."
                   (propertize (if (> tb-keycast--repeat 1)
                                   (format " x%s" tb-keycast--repeat) "")
                               'face 'tb-keycast-repeat-number-face)
-                  (propertize (if (symbolp this-command)
-                                  (symbol-name this-command)
-                                (format "%S" this-command))
+                  (propertize (format "%s" this-command))
                               'face 'tb-keycast-fun-name-face)))
 
     ;; Set min width to avoid constant jumps to right and left.
